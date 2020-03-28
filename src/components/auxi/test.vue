c0587709f987d907f6bbd6ec942a4802f020ba8b
zouzhicun <2515717245@qq.com>
2020-03-28 20:06:52 +0900

    add test

---
+++ src/components/auxi/test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="band">
+    <div style="position: relative;margin-left: 5%;margin-top: 2%;display:inline-block;width:90%;word-wrap:break-word;white-space:normal;">
+      <div>
+        <p>
+          <span style="font-weight:900;font-size:24px;">
+            {{title}} - {{playbook}}
+          </span>
+        </p>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  import util from '@/libs/util'
+
+  export default {
+    name: 'test',
+    data () {
+      return {
+        title: 'aaa',
+        playbook: 'bbb',
+      }
+    },
+    mounted () {
+    },
+    created () {
+      axios.get("http://192.168.253.128:9000/file/list?path=/tmpx")
+        .then(res => {
+          this.title=res.data['path']
+          console.log(res.data)
+        }).catch(error => {
+          util.notice(this, error, 'error')
+        })
+    }
+  }
+</script>
