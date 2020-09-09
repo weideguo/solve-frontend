ba3611ac1580baec8ec496dd3bd08e06f387d93e
zouzhicun <2515717245@qq.com>
2020-09-09 12:26:21 +0900

    update

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ let config = {}
 
 config.platformname = '简单命令分发系统'
 
-config.baseurl = [['OO测试', 'http://192.168.253.128:8000/api/v1'],['XX测试', 'http://192.168.253.128:8000/api/v1'],['docker测试', 'http://192.168.253.128:8010/api/v1'],['ZZ测试', 'https://192.168.253.128:9000/api/v1']]
+config.baseurl = [['OO测试', 'http://192.168.253.128:8000/api/v1'],['XX测试', 'http://192.168.253.128:8000/api/v1'],['ZZ测试', 'https://192.168.253.128:9000/api/v1']]
 
 export default config
