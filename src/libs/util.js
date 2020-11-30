3cb0ca03c42431825d9ade6ccbb2ec600f8fd96c
zouzhicun <2515717245@qq.com>
2020-11-30 10:37:01 +0900

    optimize

--- src/libs/util.js
+++ src/libs/util.js
@@ -4,6 +4,139 @@ import { Base64 } from 'js-base64'
 
 let util = {}
 
+// iview方法二次封装
+util.validatorGenerator = function(constrict) {
+  return function(rule, value, callback) {
+    // console.log(value, rule, constrict)
+    if ( value instanceof Date){
+      // 为时间时
+      callback()
+    }else if ((typeof constrict) === 'string') {
+      let reg = new RegExp(constrict)
+      if (value.search(reg) < 0) {
+        callback(new Error('RegExp: '+constrict))
+      } else {
+        callback()
+      }
+    } else if ( value === undefined ) {
+      callback(new Error('should not empty'))
+    } else if ( value.length === 0 ) {
+      callback(new Error('should not empty'))
+    } else if (util.existSpace(value)) {
+      callback(new Error('should no space in left and right'))
+    } else {
+      callback()
+    }
+  }
+
+}
+
+util.checkLogin = function (vm,error) {
+  // console.log(error.response)
+  if (error.response != undefined) {
+    // 网络请求时则存在状态码
+    if ( error.response.status === 401 ) {
+      // 401 权限错误
+      vm.$Message.error('Unauthorized')
+      util.openPage(vm,'login')
+    } else if ( error.response.status === 429 ) {
+      // console.log(error.response)
+      let msg=error.response.data['detail']
+      if ( msg != undefined ) {
+        vm.$Notice.error({title: 'ERROR', desc: msg})
+      } else {
+        vm.$Notice.error({title: 'ERROR', desc: error})
+      }
+    } else { 
+      let e=error.response.data
+      if ( e != '' ) {
+        error=e
+      } 
+      vm.$Notice.error({title: 'ERROR', desc: error})
+    }
+  } else {
+    vm.$Notice.error({title: 'ERROR', desc: error})
+    /*
+    try {
+      let userinfo = JSON.parse(Base64.decode(sessionStorage.getItem('jwt').split('.')[1]+'==').split(' ')[0])
+      console.log(userinfo)
+      if (userinfo['exp']*1000 < (new Date()).getTime()) {
+        console.log(userinfo['exp'])
+        vm.$Message.error('登陆已经过期，请重新登陆')
+        util.openPage(vm,'login')
+      } else {
+        vm.$Notice.error({title: 'ERROR', desc: error})
+      }
+    } catch (err) {
+      vm.$Message.error('解析jwt失败，请重新登陆')
+      util.openPage(vm,'login')
+    }
+    */
+  }
+}
+
+util.notice = function (vm, error, level) {
+  if (level === 'info') {
+    vm.$Notice.info({title: 'INFO', desc: error})
+  } else if (level === 'success') {
+    vm.$Notice.success({title: 'SUCCESS', desc: error})
+  } else if (level === 'warning') {
+    vm.$Notice.warning({title: 'WARN', desc: error})
+  } else if (level === 'error') {
+    // vm.$Notice.error({title: 'ERROR', desc: error})
+    // 在此判断登录是否过期 因为每后端请求都会有错误捕获 然后跳转到login？
+    util.checkLogin(vm,error)
+  } else if (level === 'fast') {
+    vm.$Notice.info({title: 'INFO', desc: error, duration: 1})
+  }
+}
+
+// 打开前的页面调用
+util.openPage = function (vm, name) {
+  vm.$router.push({name: name})
+  vm.$store.commit('setTagBreadBeforeOpen', name);
+}
+
+util.openPageEx = function (vm, name, query) {
+  vm.$router.push({name: name, query: query})
+  vm.$store.commit('setTagBreadBeforeOpen', name);
+}
+//////////////////////////////////////////////////////////////
+// 需要调用iview的函数
+
+// 由url下载文件
+util.download = function(vm, url, filename) {
+  axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('jwt')
+  axios.get(url, {responseType: 'blob'})
+    .then(res => {
+      util.notice(vm, filename+' downloading...', 'fast')
+      let blob = new Blob([res.data])
+      let downloadElement = document.createElement('a');
+      let href = window.URL.createObjectURL(blob);       
+      downloadElement.href = href;
+      downloadElement.download = filename;        
+      document.body.appendChild(downloadElement);
+      downloadElement.click();                           
+      document.body.removeChild(downloadElement);        
+      window.URL.revokeObjectURL(href);
+    }).catch(error => {
+      util.notice(vm, error, 'error')
+    })
+}
+
+// 复制数据到粘贴板
+util.copy = function (vm, data) {
+  let myInput = document.createElement('input');
+  myInput.value = data;
+  document.body.appendChild(myInput);
+  myInput.select();                               
+  document.execCommand('copy')
+  vm.$Message.info({'content':'path past success'})
+  document.body.removeChild(myInput);
+}
+
+//////////////////////////////////////////////////////////
+
 util.existSpace = function (s) {
   let r = s.slice(-1)
   let l = s.slice(0,1)
@@ -87,75 +220,48 @@ util.dictKeys = function (dict) {
   return keys
 },
 
-util.checkLogin = function (vm,error) {
-  // console.log(error.response)
-  if (error.response != undefined) {
-    // 网络请求时则存在状态码
-    if ( error.response.status === 401 ) {
-      // 401 权限错误
-      vm.$Message.error('Unauthorized')
-      util.openPage(vm,'login')
-    } else if ( error.response.status === 429 ) {
-      // console.log(error.response)
-      let msg=error.response.data['detail']
-      if ( msg != undefined ) {
-        vm.$Notice.error({title: 'ERROR', desc: msg})
-      } else {
-        vm.$Notice.error({title: 'ERROR', desc: error})
-      }
-    } else { 
-      let e=error.response.data
-      if ( e != '' ) {
-        error=e
-      } 
-      vm.$Notice.error({title: 'ERROR', desc: error})
-    }
-  } else {
-    vm.$Notice.error({title: 'ERROR', desc: error})
-    /*
-    try {
-      let userinfo = JSON.parse(Base64.decode(sessionStorage.getItem('jwt').split('.')[1]+'==').split(' ')[0])
-      console.log(userinfo)
-      if (userinfo['exp']*1000 < (new Date()).getTime()) {
-        console.log(userinfo['exp'])
-        vm.$Message.error('登陆已经过期，请重新登陆')
-        util.openPage(vm,'login')
-      } else {
-        vm.$Notice.error({title: 'ERROR', desc: error})
-      }
-    } catch (err) {
-      vm.$Message.error('解析jwt失败，请重新登陆')
-      util.openPage(vm,'login')
-    }
-    */
+// 序列化与反序列化
+util.formatDict = function (dict,padding='###') {
+  let result=''
+  for (let k in dict) {
+    result += padding+k+padding+'\n'+dict[k]+'\n\n'
   }
+  return result.slice(0,-2)
 }
 
-util.notice = function (vm, error, level) {
-  if (level === 'info') {
-    vm.$Notice.info({title: 'INFO', desc: error})
-  } else if (level === 'success') {
-    vm.$Notice.success({title: 'SUCCESS', desc: error})
-  } else if (level === 'warning') {
-    vm.$Notice.warning({title: 'WARN', desc: error})
-  } else if (level === 'error') {
-    // vm.$Notice.error({title: 'ERROR', desc: error})
-    // 在此判断登录是否过期 因为每后端请求都会有错误捕获 然后跳转到login？
-    util.checkLogin(vm,error)
-  } else if (level === 'fast') {
-    vm.$Notice.info({title: 'INFO', desc: error, duration: 1})
+util.parseString2Dict = function  (string,boolean_key=[],padding='###', ) {
+  let result = {}
+  let block_list = string.split('\n\n'+padding)
+  block_list[0] = block_list[0].slice(3)
+  console.log(block_list)
+  for (let i in block_list) {
+    //console.log(block_list[i])
+    let pair = block_list[i].split('###\n')
+    let k = pair[0]
+    let v =  pair[1]
+    if (boolean_key.indexOf(k)>=0) {
+      if (v === 'true' || v === '1') {
+        v= Boolean(1) 
+      }else{
+        v= Boolean(0)    
+      }
+    }
+    
+    result[k] = v
   }
+  return result
 }
+///////////////////////////////
 
-// 打开前的页面调用
-util.openPage = function (vm, name) {
-  vm.$router.push({name: name})
-  vm.$store.commit('setTagBreadBeforeOpen', name);
-}
-
-util.openPageEx = function (vm, name, query) {
-  vm.$router.push({name: name, query: query})
-  vm.$store.commit('setTagBreadBeforeOpen', name);
+// url解析参数成字典
+util.parseUrlParams = function (location) {
+  if (location.search.length <= 0) return {};
+  let info = location.search.slice(1)
+  let result = {}
+  info.split('&').forEach((item,i) => {
+    result[decodeURIComponent(item.split('=')[0])] = decodeURIComponent(item.split('=')[1])
+  })
+  return result
 }
 
 // 根据字符串以下划线分割构成树结构
@@ -246,72 +352,6 @@ util.getCheckedLeaf = function (treeData) {
 }
 ///////////////////////////
 
-util.copy = function (vm, data) {
-  let myInput = document.createElement('input');
-  myInput.value = data;
-  document.body.appendChild(myInput);
-  myInput.select();                               
-  document.execCommand('copy')
-  vm.$Message.info({'content':'path past success'})
-  document.body.removeChild(myInput);
-}
-
-// 由url下载文件
-util.download = function(vm, url, filename) {
-  axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('jwt')
-  axios.get(url, {responseType: 'blob'})
-    .then(res => {
-      util.notice(vm, filename+' downloading...', 'fast')
-      let blob = new Blob([res.data])
-      let downloadElement = document.createElement('a');
-      let href = window.URL.createObjectURL(blob);       
-      downloadElement.href = href;
-      downloadElement.download = filename;        
-      document.body.appendChild(downloadElement);
-      downloadElement.click();                           
-      document.body.removeChild(downloadElement);        
-      window.URL.revokeObjectURL(href);
-    }).catch(error => {
-      util.notice(vm, error, 'error')
-    })
-}
-
-util.validatorGenerator = function(constrict) {
-  return function(rule, value, callback) {
-    // console.log(value, rule, constrict)
-    if ( value instanceof Date){
-      // 为时间时
-      callback()
-    }else if ((typeof constrict) === 'string') {
-      let reg = new RegExp(constrict)
-      if (value.search(reg) < 0) {
-        callback(new Error('RegExp: '+constrict))
-      } else {
-        callback()
-      }
-    } else if ( value === undefined ) {
-      callback(new Error('should not empty'))
-    } else if ( value.length === 0 ) {
-      callback(new Error('should not empty'))
-    } else if (util.existSpace(value)) {
-      callback(new Error('should no space in left and right'))
-    } else {
-      callback()
-    }
-  }
-
-}
-
-util.parseUrlParams = function (location) {
-  if (location.search.length <= 0) return {};
-  let info = location.search.slice(1)
-  let result = {}
-  info.split('&').forEach((item,i) => {
-    result[decodeURIComponent(item.split('=')[0])] = decodeURIComponent(item.split('=')[1])
-  })
-  return result
-}
-
 //文件操作
 util.write = function (string, filename, filetype='text/plain') {
   var blob;
@@ -365,37 +405,6 @@ util.read = function (blobfile,accepttype=['text/plain']) {
     }
   });
 }
-
-// 序列化与反序列化
-util.formatDict = function (dict,padding='###') {
-  let result=''
-  for (let k in dict) {
-    result += padding+k+padding+'\n'+dict[k]+'\n\n'
-  }
-  return result.slice(0,-2)
-}
-
-util.parseString2Dict = function  (string,boolean_key=[],padding='###', ) {
-  let result = {}
-  let block_list = string.split('\n\n'+padding)
-  block_list[0] = block_list[0].slice(3)
-  console.log(block_list)
-  for (let i in block_list) {
-    //console.log(block_list[i])
-    let pair = block_list[i].split('###\n')
-    let k = pair[0]
-    let v =  pair[1]
-    if (boolean_key.indexOf(k)>=0) {
-      if (v === 'true' || v === '1') {
-        v= Boolean(1) 
-      }else{
-        v= Boolean(0)    
-      }
-    }
-    
-    result[k] = v
-  }
-  return result
-}
+///////////////////////////////
 
 export default util
