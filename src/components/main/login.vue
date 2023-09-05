9bab281e648f4004eb7c107ee2fcd7d15aefd317
weideguo <2515717245@qq.com>
2023-09-05 19:47:18 +0800

    chore

--- src/components/main/login.vue
+++ src/components/main/login.vue
@@ -88,7 +88,7 @@
 
 
         <p style="margin-left: 25%;">
-          2018-2022 © Powerd By <a href="/about">wdg</a> {{ $t('loginTips') }}
+          2018-2023 © Powerd By <a href="/about">wdg</a> {{ $t('loginTips') }}
         </p>
 
       </Card>
